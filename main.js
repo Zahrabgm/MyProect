79351dfcf6ec285008b1c4fba23c37945c840a38
Xuyen <thi.vu@dci-student.org>
2025-05-06 13:54:02 +0200

    xuyen update

--- main.js
+++ main.js
@@ -72,19 +72,3 @@ fetch(`https://fakestoreapi.com/products`)
             });
         });
     }
-
-fetch ('https://fakestoreapi.com/products/categories').then(Response =>{
-    if(!Response.ok) {
-        throw new Error("there is an error");    
-    }
-    return Response.json();
-})
-.then(data =>  {
-    
- console.log(data)
-})
-.catch(erro =>{
-    console.error(`Fetch error`)
-    document.querySelector(`.nav-search.border`).innerHTML = `Something went wrong!`
-})
-
