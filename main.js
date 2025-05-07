03ad2d0c285284ce1f71cb0c36493c8cd9536e52
Zahrabgm <zahrabegum91@gmail.com>
2025-05-07 12:02:54 +0200

    add

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
 // Categories 
 
-
-/* Language section*/
-/*document.getElementById('languageSelector').addEventListener('change', (event) => {
-    const selectedLanguage = event.target.value;
-
-    // Perform an action based on the selected language
-    switch (selectedLanguage) {
-        case 'en':
-            console.log('Language changed to English');
-            document.body.innerHTML += `<p>Language changed to English</p>`;
-            break;
-        case 'de':
-            console.log('Language changed to Deutsch');
-            document.body.innerHTML += `<p>Sprache geändert zu Deutsch</p>`;
-            break;
-        default:
-            console.log('Language not supported');
-            document.body.innerHTML += `<p>Language not supported</p>`;
-    }
-});*/
-
 // Products section
 const boxSec = document.querySelector(".box-sec");
 const morebtn = document.querySelector(".more-btn");
@@ -82,6 +61,8 @@ fetch(`https://fakestoreapi.com/products`)
         });
     }
 
+    /*catageries*/
+    const searchBar = document.querySelector ('search-select')
 fetch ('https://fakestoreapi.com/products/categories').then(Response =>{
     if(!Response.ok) {
         throw new Error("there is an error");    
@@ -92,11 +73,12 @@ fetch ('https://fakestoreapi.com/products/categories').then(Response =>{
 
  console.log(data)
 
- const resultsContainer = document.querySelector(`.nav-search border`)
- resultsContainer.innerHTML += `<p>Result: ${JSON.stringify(data)}</p>`
-})
-.catch(erro =>{
-    console.error(`Fetch error`)
+ // const resultsContainer = document.querySelector(`.nav-search border`)
+ data.forEach((e) => { searchBar.innerHTML += `<option value="">${e}</option>`})
+
+ })
+.catch(error =>{
+    console.error(`Fetch error`,error)
     document.querySelector(`.nav-search.border`).innerHTML += `<p>Something went wrong!<p/>`
 })
 
@@ -120,5 +102,3 @@ document.getElementById('languageSelector').addEventListener('change', (event) =
             document.body.innerHTML += `<p>Language not supported</p>`;
     }
 });
-  
-    }
